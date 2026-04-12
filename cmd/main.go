12a2a1c52e2b49fc45592eea4c2fe082a7ca3261
Dmitry Ponomaryov <iamhalje@gmail.com>
2026-04-12 23:11:47 +0500

    sync
    Signed-off-by: Dmitry Ponomaryov <iamhalje@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -104,7 +104,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "57410f0d.maxim.technology",
+		LeaderElectionID:       "57410f0d.halje.ru",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
